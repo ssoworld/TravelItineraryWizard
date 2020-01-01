66f4c0f0730bde941fc807913bc436e37961ecec
Scott Onson <onsons@ssoworld.com>
2020-01-01 13:21:59 -0600

    starting - it doesn't work yet :zzz:

---
+++ Source/CarRentalHertz.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TravelItineraryWizard
+{
+   class CarRentalHertz : ICarRentalAgency
+   {
+      public Dictionary<string, int> _cboxdata;
+
+      public Dictionary<string, int> ComboBoxData => throw new NotImplementedException();
+
+      public CarRentalHertz()
+      {
+         _cboxdata = new Dictionary<string,int>();
+      }
+
+      /// <summary>
+      /// Car classes as defined by agency Hertz Car Rental
+      /// </summary>
+      public enum CarClasses
+      {
+         [Description(description: "Economy 2/4 Door (A)")]
+         CarEconomy,
+         [Description(description: "Compact 2/4 Door (B)")]
+         CarCompact,
+         [Description(description: "Intermediate 2/4 Door (C)")]
+         CarIntermediate,
+         [Description(description: "Standard 2/4 Door (D)")]
+         CarStandard,
+         [Description(description: "Standard Sport (D2)")]
+         CarStandardSport,
+         [Description(description: "Standard Elite 2/4 Door (P4)")]
+         CarStandardElite,
+         [Description(description: "Fullsize 2/4 Door (F)")]
+         CarFullsize,
+         [Description(description: "Premium 2/4 Door (G)")]
+         CarPremium,
+         [Description(description: "Luxury 2/4 Door (I)")]
+         CarLuxury,
+         [Description(description: "Luxury Sport (G4)")]
+         CarLuxurySport,
+         [Description(description: "Compact SUV (B4)")]
+         SUVCompact,
+         [Description(description: "Compact SUV GMMP (Y6)")]
+         SUVCompactGMMP,
+         [Description(description: "Intermediate SUV (Q4)")]
+         SUVIntermediate,
+         [Description(description: "Intermediate Elite SUV (H4)")]
+         SUVIntermediateElite,
+         [Description(description: "Standard SUV (L)")]
+         SUVStandard,
+         [Description(description: "Standard SUV GMMP (Y7)")]
+         SUVStandardGMMP,
+         [Description(description: "Standard Elite SUV (T4)")]
+         SUVStandardElite,
+         [Description(description: "Fullsize SUV (T)")]
+         SUVFullsize,
+         [Description(description: "Fullsize SUV GMMP (Y8)")]
+         SUVFullsizeGMMP,
+         [Description(description: "Fullsize SUV (V6)")]
+         SUVPremium,
+         [Description(description: "Luxury SUV (P6)")]
+         SUVLuxuryP6,
+         [Description(description: "Luxury SUV (S6)")]
+         SUVLuxuryS6,
+         [Description(description: "Standard Elite Crossover (Z4)")]
+         SUVCrossoverStandardElite,
+         [Description(description: "Mini Passenger Van (R)")]
+         VanPassMini,
+         [Description(description: "Fullsize Passenger Van (M)")]
+         VanPassFullsize,
+         [Description(description: "Standard Convertible (U)")]
+         ConvertibleStandard,
+         [Description(description: "Standard Convertible (U4)")]
+         ConvertibleEliteStandard,
+         [Description(description: "Intermediate Open Air All Terrain (V)")]
+         AllTerrainIntermediate,
+      }
+
+      /// <summary>
+      /// 
+      /// </summary>
+      public void LoadDictionary()
+      {
+         foreach(int enumValue in Enum.GetValues(typeof(CarClasses)))
+         {
+            _cboxdata.Add(Enum.GetName(typeof(CarClasses), enumValue), enumValue);
+         }
+      }
+   }
+}
---
+++ Source/WizardShell.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+
+namespace TravelItineraryWizard
+{
+   public partial class WizardShell
+   {
+      private void WizardShell_Startup(object sender, System.EventArgs e)
+      {
+      }
+
+      private void WizardShell_Shutdown(object sender, System.EventArgs e)
+      {
+      }
+
+      #region VSTO generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InternalStartup()
+      {
+         this.Startup += new System.EventHandler(WizardShell_Startup);
+         this.Shutdown += new System.EventHandler(WizardShell_Shutdown);
+      }
+
+      #endregion
+   }
+}
